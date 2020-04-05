16d43d522a4f7fd751e026bf8678d6b5961b275e
treyboyland <treyboyland@aol.com>
2020-04-05 15:40:27 -0500

    Removing cursor during gameplay

---
+++ Assets/Scripts/CursorToggle.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorToggle : MonoBehaviour
+{
+    [SerializeField]
+    bool showCursor;
+
+    private void OnEnable()
+    {
+        if (gameObject.activeInHierarchy)
+        {
+            Cursor.visible = showCursor;
+        }
+    }
+}
